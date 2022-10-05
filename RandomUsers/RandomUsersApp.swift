4449a385803249fa2a27d9ea8a222b0658acf70d
krwi20 <kristoffwittwer@icloud.com>
2022-10-05 16:43:34 +0100

    init

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Kris Wittwer on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
