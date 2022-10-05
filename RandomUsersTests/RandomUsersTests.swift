1f54243d9cffaa1e795800dd68099199ce162859
krwi20 <kristoffwittwer@icloud.com>
2022-10-05 19:29:34 +0100

    Testing the user model

--- RandomUsersTests/RandomUsersTests.swift
+++ RandomUsersTests/RandomUsersTests.swift
@@ -9,6 +9,25 @@ import XCTest
 @testable import RandomUsers
 
 final class RandomUsersTests: XCTestCase {
+    
+    func testUserModel() throws {
+        let jsonData = getTestJSONData()
+        
+        do {
+            _ = try JSONDecoder().decode(Response.self, from: jsonData)
+        }
+        catch {
+            XCTFail("Failed to decode JSON into the model : \(error)")
+        }
+    }
+    
+    private func getTestJSONData() -> Data {
+        guard let path = Bundle.main.path(forResource: "randomUsers", ofType: "json") else {
+            fatalError("randomUsers.json file not found")
+        }
+        let internalURL = URL(fileURLWithPath: path)
+        return try! Data(contentsOf: internalURL)
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
